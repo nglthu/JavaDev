166c708e471a36cccc9a57dd9071999969279b19
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-09-20 07:39:03 +0000

    List of student

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,30 @@
  *---------------------------------------------------------------------------------------*/
 
 package com.mycompany.app;
+import java.util.ArrayList;
+
+import com.mycompany.app.student.Student;
+
+
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+        
+        ArrayList<Student> lsv = new ArrayList<Student>();
+
+
+        Student sv1= new Student();
+        
+        sv1.fullname = "Nguyen";
+        sv2.studentID = "masv123";
+       // Student sv2 = new Student('dddeee', 'ddde34444');
+
+    lsv.add(sv1);
+   // lsv.add(sv2);
+
+    System.out.println(lsv.get(0).fullname);
+
+
     }
 }
---
+++ src/main/java/com/mycompany/app/student/Student.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.student;
+
+public class Student {
+
+    String fullname;
+    String studentID;
+
+    Student(){
+        
+    }
+
+    Student (String name, String id){
+        this.fullname = name;
+        this.studentID = id;
+
+    }
+}
